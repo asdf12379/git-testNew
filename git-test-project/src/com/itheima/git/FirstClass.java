435c77c1e79893b438fd9fe5682bc87b15318646
“asdf12379 <3224349910@qq.com>
2020-10-10 21:22:54 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,19 @@
+package com.itheima.git;
+
+/**
+ * @author chenming
+ * @date 2020/10/10 -  21:04
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+
+    }
+
+}
